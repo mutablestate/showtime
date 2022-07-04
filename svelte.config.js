5e32d6ab31acc8b191290794372cf074344343d2
mutablestate <benwebdev@gmail.com>
2022-07-04 12:56:36 +0700

    initial commit

---
+++ svelte.config.js
@@ -0,0 +1,22 @@
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+
+export default {
+	preprocess: preprocess(),
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			trailingSlash: 'always'
+		}),
+
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
+	}
+};
